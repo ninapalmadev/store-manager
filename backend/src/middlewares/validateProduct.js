689956e789ca48e720ebfed1b3014f44fbfe0256
Nina Palma <ninapalmadev@gmail.com>
2023-09-15 16:56:08 -0300

    corrige lógica de validação do name

--- backend/src/middlewares/validateProduct.js
+++ backend/src/middlewares/validateProduct.js
@@ -1,10 +1,9 @@
 const validateProduct = (req, res, next) => {
   const { name } = req.body;
-  const length = name < 5;
-  if (!name) {
+  if (name === undefined) {
     return res.status(400).json({ message: '"name" is required' });
   }
-  if (!length) {
+  if (name.length < 5) {
     return res.status(422).json(
       { message: '"name" length must be at least 5 characters long' },
       );
