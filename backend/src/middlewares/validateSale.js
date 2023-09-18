203cc0bed2d8b005acb9b6022153fcb39751371e
Nina Palma <ninapalmadev@gmail.com>
2023-09-18 11:55:21 -0300

    requisito 6

---
+++ backend/src/middlewares/validateSale.js
@@ -0,0 +1,41 @@
+const { productsModel } = require('../models');
+
+const validateId = async (req, res, next) => {
+  const products = await productsModel.getAll();
+  const ids = products.map(({ id }) => id);
+
+  const { body } = req;
+  const saleId = body.map(({ productId }) => productId);
+
+  const validate = saleId.some((item) => item === undefined);
+  const validateProductId = saleId.every((item) => ids.includes(item));
+
+  if (validate) {
+    return res.status(400).json({ message: '"productId" is required' });
+  }
+  if (!validateProductId) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+  next();
+};
+
+const validateQuantity = async (req, res, next) => {
+  const { body } = req;
+  const saleQuantity = body.map(({ quantity }) => quantity);
+
+  const validate = saleQuantity.some((item) => item === undefined);
+  const validateQnty = saleQuantity.every((item) => item > 0);
+
+  if (validate) {
+    return res.status(400).json({ message: '"quantity" is required' });
+  }
+  if (!validateQnty) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+  next();
+};
+
+module.exports = {
+  validateId,
+  validateQuantity,
+};
--- backend/src/routes/sales.route.js
+++ backend/src/routes/sales.route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
 const { salesController } = require('../controllers');
+const { validateId, validateQuantity } = require('../middlewares/validateSale');
 
 router.get('/', salesController.getAll);
 router.get('/:id', salesController.getById);
-router.post('/', salesController.create);
+router.post('/', validateId, validateQuantity, salesController.create);
 
 module.exports = router;
